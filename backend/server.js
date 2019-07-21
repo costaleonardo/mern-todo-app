7a5c370434ac87d05fe9bc3337469f6f2bdda68f
Leonardo Da Costa <costaleonardo97@gmail.com>
2019-07-21 18:00:03 -0400

    Add Todo model.

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
 const cors = require('cors');
 
@@ -8,5 +9,12 @@ const app = express();
 
 app.use(cors());
 app.use(bodyParser.json());
+
+mongoose.connect('mongodb://127.0.0.1:27017/todos', { useNewUrlParser: true });
+const connection = mongoose.connection;
+
+connection.once('open', () => {
+  console.log('MongoDB database connection established successfully.');
+});
 
 app.listen(PORT, () => console.log(`Seerver running on port ${PORT}`));
